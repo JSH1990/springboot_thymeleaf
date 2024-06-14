69726301a8aa8c0c2e469c9da56745a2d223d498
DESKTOP-SN9D63J\JSH <t>
2024-06-14 17:25:58 +0900

    //first

---
+++ src/main/java/com/black/spring_thymeleaf_pratice/SpringThymeleafPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.black.spring_thymeleaf_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/black/spring_thymeleaf_pratice/SpringThymeleafPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.spring_thymeleaf_pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
